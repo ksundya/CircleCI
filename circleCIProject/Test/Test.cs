2523e5e564e6dd06b5c15b44d3d7da59486fa172
Kseniya Dunchyk <Kseniya_Dunchyk@epam.com>
2019-10-09 11:22:36 +0300

    modifying config.yml

--- circleCIProject/Test/Test.cs
+++ circleCIProject/Test/Test.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace CircleCIProject.Test
 {
-  public class Test
-    {
-
-
+    public class Test { 
     }
 }
